bf1a2ee065309bf3f382acfbeb3ea65880373827
Pedro Miguel Fernandes <pedro@pmfernandes.com>
2019-11-12 23:01:34 +0000

    feat: Add Client Project

---
+++ client/Program.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Core;
+using System;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountServiceRequest();
+
+            ProductServiceRequest();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+
+        static void AccountServiceRequest()
+        {
+            try
+            {
+                Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+                var client = new AccountService.AccountServiceClient(channel);
+                EmployeeName empName = client.GetEmployeeName(new EmployeeNameRequest { EmpId = "1" });
+                if (empName == null || string.IsNullOrWhiteSpace(empName.FirstName) || string.IsNullOrWhiteSpace(empName.LastName))
+                {
+                    Console.WriteLine("Employee not found.");
+                }
+                else
+                {
+                    Console.WriteLine($"The employee name is {empName.FirstName} {empName.LastName}.");
+                }
+                channel.ShutdownAsync().Wait();
+                Console.WriteLine("AccountServiceRequest Terminated...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception encountered: {ex}");
+            }
+        }
+
+        static void ProductServiceRequest()
+        {
+            try
+            {
+                var productServiceChannel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+                var productServiceClient = new ProductService.ProductServiceClient(productServiceChannel);
+                var product = productServiceClient.GetProductById(new ProductByIdRequest { ProductId = 1 });
+                Console.WriteLine($"ProductUuid: {product.ProductUuid}");
+
+                var product2 = productServiceClient.GetProductByUuid(new ProductByUuidRequest { ProductUuid = "Id Of Product 1"});
+                Console.WriteLine($"ProductId: {product.ProductId}");
+
+                productServiceChannel.ShutdownAsync().Wait();
+                Console.WriteLine("ProductServiceRequest Terminated...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception encountered: {ex}");
+            }
+        }
+    }
+}
